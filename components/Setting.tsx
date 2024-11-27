f2187dcba51036fbc28fd0e57216ce86223b39b9
SILINOU NICOLAS <silinou@et.esiea.fr>
2024-11-27 15:59:58 +0100

    Les notifications push arrivent bien et la base de données Cloud_Firestore test est bien configurée

--- components/Setting.tsx
+++ components/Setting.tsx
@@ -1,14 +1,23 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { getUsers } from '../firebase';
 
 const Setting = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const users = await getUsers();
+      console.log('Users in Settings:', users);
+    };
+    fetchData();
+  }, []);
+
   return (
     <SafeAreaView>
       <Text>Setting</Text>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Setting
+export default Setting;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
