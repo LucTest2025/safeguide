bab32a5fddf694acc94b7c4ac0355919e5e58419
SILINOU NICOLAS <silinou@et.esiea.fr>
2024-11-28 14:12:21 +0100

    Changement des fichiers .tsx en .js et configuration de la fonction pour mise à jour de refuges : Réussi

--- components/Home.js
+++ components/Home.js
@@ -2,16 +2,8 @@ import { SafeAreaView, StyleSheet, Text, Button, FlatList } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { addUser, getUsers } from '../firebase';
 
-// Définir le type d'utilisateur
-type User = {
-  id: string;
-  name: string;
-  role: string;
-};
-
 const Home = () => {
-  // Préciser le type de l'état users
-  const [users, setUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     // Récupérer les utilisateurs au chargement
@@ -19,7 +11,7 @@ const Home = () => {
   }, []);
 
   const fetchUsers = async () => {
-    const usersData = await getUsers(); 
+    const usersData = await getUsers();
     setUsers(usersData || []); // Initialiser avec un tableau vide si usersData est null/undefined
   };
 
@@ -29,7 +21,7 @@ const Home = () => {
       <Button title="Add User" onPress={() => addUser('Luc Aymar', 'Developer')} />
       <FlatList
         data={users}
-        keyExtractor={item => item.id}
+        keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <Text>{item.name} - {item.role}</Text>
         )}
